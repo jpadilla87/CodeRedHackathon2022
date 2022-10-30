2072d6bc48fb1779b76b4ec63f32387151687234
earmando29 <earmando29@gmail.com>
2022-10-29 22:26:30 -0500

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const fetch = require("node-fetch");
+
+const URL = "https://worldpopulationreview.com/state-rankings/oil-production-by-state";
+
+fetch( URL, {
+    mode: 'cors',
+    headers: {
+        'Access-Control-Allow-Origin':'*'
+    }
+})
+
+.then(response => response.text())
+.then(data => console.log(data.substring(data.indexOf("Oil"))))
+
